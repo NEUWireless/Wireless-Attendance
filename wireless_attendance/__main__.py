595e3b762acf6a1e6a03ef52480278d4c905b61d
Brian Schubert <brianm.schubert@gmail.com>
2020-01-31 14:08:42 -0500

    Update help text for command line interface

--- wireless_attendance/__main__.py
+++ wireless_attendance/__main__.py
@@ -16,21 +16,41 @@ logger = logging.getLogger(__name__)
 def make_parser() -> argparse.ArgumentParser:
     """Construct the argument parser for this script."""
     parser = argparse.ArgumentParser(
-        description='Wireless Attendance Tracking'
+        description="Wireless Attendance Tracking",
+        prog='wireless_attendance',
     )
 
-    parser.add_argument('--credentials-file', type=str,
-        help="""The path to the Google Service Account credentials
+    parser.add_argument(
+        '--credentials-file',
+        type=str,
+        help="""The path to the Google Service Account credentials.
         See the gspread documentation for information on how to obtain a credentials file: https://gspread.readthedocs.io/en/latest/oauth2.html
         """,
         default=settings.DEFAULT_GOOGLE_CREDENTIALS_FILE,
     )
 
     spreadsheet_id_group = parser.add_mutually_exclusive_group(required=True)
-    spreadsheet_id_group.add_argument('--spreadsheet-id', type=str)
-    spreadsheet_id_group.add_argument('--spreadsheet-name', type=str)
-    spreadsheet_id_group.add_argument('--spreadsheet-url', type=str)
-    spreadsheet_id_group.add_argument('-no-sheet', action='store_true')
+    spreadsheet_id_group.add_argument(
+        '--spreadsheet-id',
+        type=str,
+        help="The ID of the spreadsheet to store attendance data."
+    )
+    spreadsheet_id_group.add_argument(
+        '--spreadsheet-name',
+        type=str,
+        help="The name of the spreadsheet to store attendance data.",
+    )
+    spreadsheet_id_group.add_argument(
+        '--spreadsheet-url',
+        type=str,
+        help="The complete URL to the spreadsheet to store attendance data.",
+    )
+    spreadsheet_id_group.add_argument(
+        '-no-sheet',
+        action='store_true',
+        help="If specified, the process will not attempt to connect to the "
+             "Google Sheets API. All card reads will still be written the the logger."
+    )
 
     parser.add_argument('-mock-reader', action='store_true')
 
