a62d4a56afcde10a0714a32a1a76a346b937209c
Brian Schubert <brianm.schubert@gmail.com>
2020-02-04 22:50:17 -0500

    Add doc comments for settings module

--- wireless_attendance/settings.py
+++ wireless_attendance/settings.py
@@ -1,3 +1,7 @@
+"""
+Package settings.
+"""
+
 import os
 from datetime import timedelta
 
@@ -18,16 +22,41 @@ WORKSHEETS = {
         'columns': ['Member Name', 'Total Meetings']
     }
 }
+"""
+Dictionary describing the structure the the Google Sheets spreadsheets
+that this package interfaces with.
+"""
 
 CARD_READER_REPEAT_TIMEOUT = timedelta(seconds=10)
+"""
+The period of time that must be elapsed between two instances of the same
+card UUID being read.
+
+Repeated reads that occur in less than this amount of time will be ignored.
+"""
 
 CARD_READER_READ_TIMEOUT = timedelta(seconds=1)
+"""
+The duration of time for which the card reader will search for a card before
+concluding that no card was found.
+"""
 
 CARD_READER_DELAY = timedelta(seconds=0.5)
+"""
+The duration of time that the process will remain idle between attempted card
+reads.
+"""
 
 DEFAULT_GOOGLE_CREDENTIALS_FILE = os.path.join(BASE_DIR, 'wireless-attendance-credentials.json')
+"""
+The default location to search for the google credentials file if no credentials
+file is specified via the command line.
+"""
 
 LOGGING_FILE = os.path.join(BASE_DIR, 'wireless-attendance.log')
+"""
+The path to the log file used by this package.
+"""
 
 LOGGING = {
     'version': 1,
